0551b061c68fbc29a02d987204635dd39e30968c
Ayra <oscar.ayras@gmail.com>
2020-06-09 03:10:21 -0500

    Changing path of environments

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -5,4 +5,5 @@ echo "############# Testing ##############"
 echo "####################################"
 
 
-docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
+#docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
+docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/lib/docker/volumes/jenkins-data/_data/workspace/pipeline-maven-docker/java-app:/app -w /app maven:alpine "$@"
