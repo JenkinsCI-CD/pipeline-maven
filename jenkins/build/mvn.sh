ba76a9df8d65af827ab47f06bbf49256919696f8
Ayra <oscar.ayras@gmail.com>
2020-06-09 19:13:37 -0500

    Fix the name of the project

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,4 +5,4 @@ echo "********MVN build .jar*********"
 echo "*****************************"
 
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
-docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-maven-docker/java-app:/app -w /app maven:alpine "$@"
+docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:alpine "$@"
