b2403bd6773bcc62b3b373f093455d4774a149bd
Ayra <oscar.ayras@gmail.com>
2020-06-11 01:30:01 -0500

    docker run with user=jenkins

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,5 +6,6 @@ echo "*****************************"
 
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:alpine "$@"
-docker run --rm -u jenkins -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/lib/jenkins/workspace/pipe-docker-maven-cicd/java-app:/app -w /app maven:alpine "$@"
+docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/lib/jenkins/workspace/pipe-docker-maven-cicd/java-app:/app -w /app maven:alpine "$@"
+
 
