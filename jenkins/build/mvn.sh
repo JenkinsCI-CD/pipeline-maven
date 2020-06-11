2f5805910be66e021855318cac2f50183b5d81ea
Ayra <oscar.ayras@gmail.com>
2020-06-10 20:10:14 -0500

    Fix some changes

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,4 +5,5 @@ echo "********MVN build .jar*********"
 echo "*****************************"
 
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
-docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:alpine "$@"
+#docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:alpine "$@"
+docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipe-docker-maven-cicd/java-app:/app -w /app maven:alpine "$@"
