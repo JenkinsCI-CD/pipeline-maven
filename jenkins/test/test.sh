404f550cef8dc1165458d7866646e5707ba350d3
Ayra <oscar.ayras@gmail.com>
2020-06-11 00:43:59 -0500

    Change paths of files

--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -7,4 +7,4 @@ echo "####################################"
 
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v $HOME/workspace-personal/jenkins/pipeline-project/java-app:/app -w /app maven:alpine "$@"
 #docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/lib/docker/volumes/jenkins-data/_data/workspace/pipeline-maven-docker/java-app:/app -w /app maven:alpine "$@"
-docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipe-docker-maven-cicd/java-app:/app -w /app maven:alpine "$@"
+docker run --rm -v $HOME/docker-volumes/.m2:/root/.m2 -v /var/lib/jenkins/workspace/pipe-docker-maven-cicd/java-app:/app -w /app maven:alpine "$@"
