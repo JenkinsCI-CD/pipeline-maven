d9abd8e4e2064db01c4de204397d4e5a489c9c52
Ayra <oscar.ayras@gmail.com>
2020-06-11 01:36:31 -0500

    removing /opt/

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -6,6 +6,6 @@ echo $BUILD_TAG >> /tmp/.envs
 #echo $BUILD_PASS >> /tmp/.envs
 
 # Transfiere el archivo
-scp -i /opt/id_rsa /tmp/.envs ayraserver@52.188.125.65:/tmp/.envs
-scp -i /opt/id_rsa ./jenkins/deploy/publish ayraserver@52.188.125.65:/tmp/publish
-ssh -i /opt/id_rsa ayraserver@52.188.125.65 /tmp/publish
+scp /tmp/.envs ayraserver@52.188.125.65:/tmp/.envs
+scp ./jenkins/deploy/publish ayraserver@52.188.125.65:/tmp/publish
+ssh ayraserver@52.188.125.65 /tmp/publish
